d8a74c955368dde84fbd1621286a40ac3c3e4c52
Elliot <jorgeispython@gmail.com>
2025-09-26 12:13:22 -0500

    prueba-merg

--- obj/Debug/net9.0/Lab01-Grupal.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lab01-Grupal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01-Grupal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e29c6d0af0cce475f22c1024bf5be6b83427b6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+492cca8c0cb132ebd4aad17c52f62a0c3570a20c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01-Grupal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01-Grupal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
