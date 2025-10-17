0141ca7affa7c9107ba81d896f934ec1d1e580b8
jeanfv09 <jeanfrancov09@gmail.com>
2025-10-17 13:59:50 -0500

    Añadiendo el registro de contactos que ha sido eliminado y arreglando algunas cosas

--- obj/Debug/net9.0/Lab01-Grupal.AssemblyInfo.cs
+++ obj/Debug/net9.0/Lab01-Grupal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab01-Grupal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a526547b3b5224dc6533478ea43ccee8c2b49dae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dff6e28f62817780b0dc0f0c4489f62ce23e753")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab01-Grupal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab01-Grupal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
